42f0ed2b53650af584d4c041ebb67b7d45a87044
Shadab Zafar <dufferzafar0@gmail.com>
2014-05-24 10:50:48 +0530

    PEP 8 Code + MD5 Hashes in Gen JSON
    Changed the script to use PEP 8 compliant code with 4 space
    indentation and other fixes.
    
    A subprocess.call("git pull -q") to update our repo from github.
    
    It contains MD5 Hashes of all files (which will be used to check if
    a file has been modified or not.)
    
    All files are scanned for plugin data and not just __init__.py
    
    The JSON output is now dumped to a file rather than the screen.

--- Gen_Json.py
+++ Gen_Json.py
@@ -1,63 +1,68 @@
 #!python3
 
 # Imports
-import os, json, re
+import os
+import json
+import re
+import hashlib
+import subprocess
 
-# Used to extract information from plugin files
 def getPluginData(filePath):
-  plugData = {}
+    """
+    Extract usable information from plugin files.
+    """
+    plugData = {}
 
-  with open(filePath) as f:
-    for line in f:
-      name = re.match(r'PLUGIN_NAME = (.*)', line)
-      author = re.match(r'PLUGIN_AUTHOR = (.*)', line)
-      desc = re.match(r'PLUGIN_DESCRIPTION = (.*)', line)
-      ver = re.match(r'PLUGIN_VERSION = (.*)', line)
-      apiver = re.match(r'PLUGIN_API_VERSIONS = (.*)', line)
+    with open(filePath) as f:
+        for line in f:
+            name = re.match(r'PLUGIN_NAME = (.*)', line)
+            author = re.match(r'PLUGIN_AUTHOR = (.*)', line)
+            desc = re.match(r'PLUGIN_DESCRIPTION = (.*)', line)
+            ver = re.match(r'PLUGIN_VERSION = (.*)', line)
+            apiver = re.match(r'PLUGIN_API_VERSIONS = (.*)', line)
 
-      if name:
-        plugData['title'] = name.group(1)
-      if author:
-        plugData['author'] = author.group(1)
-      if desc:
-        plugData['desc'] = desc.group(1)
-      if ver:
-        plugData['ver'] = ver.group(1)
-      if apiver:
-        plugData['apiver'] = apiver.group(1)
+            if name:
+                plugData['title'] = name.group(1)
+            if author:
+                plugData['author'] = author.group(1)
+            if desc:
+                plugData['desc'] = desc.group(1)
+            if ver:
+                plugData['ver'] = ver.group(1)
+            if apiver:
+                plugData['apiver'] = apiver.group(1)
 
-  return plugData
+    return plugData
 
-# Constants
 plug_dir = "Plugins"
-
-# Variables
 plugins = []
 
-# Walk in a park
+# Pull contents from Github
+# subprocess.call(["git", "pull", "-q"])
+
+# Traverse the plugins dir to create JSON
 for dirName in os.listdir(plug_dir):
 
-  files = []
+    files = {}
+    data = {}
 
-  for fileName in os.listdir(os.path.join(plug_dir, dirName)):
-    ext = os.path.splitext(fileName)[1]
+    for fileName in os.listdir(os.path.join(plug_dir, dirName)):
+        ext = os.path.splitext(fileName)[1]
 
-    if ext not in [".pyc"]:
-      files.append(fileName)
+        if ext not in [".pyc"]:
+            files[fileName] = hashlib.md5(open(os.path.join(plug_dir, dirName,
+                                            fileName), "rb").read()).hexdigest()
 
-      if fileName == "__init__.py":
-        data = getPluginData(os.path.join(plug_dir, dirName, fileName))
+        if not data:
+            data = getPluginData(os.path.join(plug_dir, dirName, fileName))
 
+    if data:
+        data['id'] = dirName
+        data['files'] = files
+        plugins.append(data)
 
-  if data:
-    data['name'] = dirName
-    data['files'] = files
-    plugins.append(data)
+    else:
+        plugins.append({"id": dirName, "files": files})
 
-  else:
-    plugins.append({
-      "name" : dirName,
-      "files" : files
-      })
-
-print(json.dumps({ "plugins": plugins }, sort_keys=True, indent=2))
+# print(json.dumps({ "plugins": plugins }, sort_keys=True, indent=2))
+json.dump({"plugins": plugins}, open("Plugins.json", "w"), sort_keys=True, indent=2)
