9030ab885a0055e0fd240c4e7fa7cef3a7ce2385
Shadab Zafar <dufferzafar0@gmail.com>
2014-08-10 03:22:50 +0530

    Updated the generate script
    Fixed a critical bug in the regex matching.
    
    Converted all camelCase names to under_score.
    
    Changed the path of plugin_dir and plugin_file.
    
    Better print output.

--- generate.py
+++ generate.py
@@ -11,56 +11,56 @@ import zlib
 from hashlib import md5
 from subprocess import call
 
-reName = re.compile(r'PLUGIN_NAME = (?:_\(u|u)((?:\"\"\"|\'\'\'|\"|\'))(.*)\1')
-reAuthor = re.compile(r'PLUGIN_AUTHOR = (?:_\(u|u)((?:\"\"\"|\'\'\'|\"|\'))(.*)\1')
-reVer = re.compile(r'PLUGIN_VERSION = (?:_\(u|u)((?:\"\"\"|\'\'\'|\"|\'))(.*?)\1')
-reAPI = re.compile(r'PLUGIN_API_VERSIONS = \[((?:\"\"\"|\'\'\'|\"|\'))(.*?)\1\]')
+re_name = re.compile(r'PLUGIN_NAME = (?:_\(u|u|)((?:\"\"\"|\'\'\'|\"|\'))(.*)\1')
+re_author = re.compile(r'PLUGIN_AUTHOR = (?:_\(u|u|)((?:\"\"\"|\'\'\'|\"|\'))(.*)\1')
+re_ver = re.compile(r'PLUGIN_VERSION = (?:_\(u|u|)((?:\"\"\"|\'\'\'|\"|\'))(.*?)\1')
+re_api = re.compile(r'PLUGIN_API_VERSIONS = \[((?:\"\"\"|\'\'\'|\"|\'))(.*?)\1\]')
 
 # Descriptions are spread out in multiple lines so these will be handled separately
-reDescStart = re.compile(r'PLUGIN_DESCRIPTION = (?:_\(u|u)(.*)')
-reDescEnd = re.compile(r'PLUGIN_(.*)')
-reDesc = re.compile(r'PLUGIN_DESCRIPTION = (?:_\(u|u)((?:\"\"\"|\'\'\'|\"|\'))(.*?)\1', re.DOTALL)
+re_desc_start = re.compile(r'PLUGIN_DESCRIPTION = (?:_\(u|u|)(.*)')
+re_desc_end = re.compile(r'PLUGIN_(.*)')
+re_desc = re.compile(r'PLUGIN_DESCRIPTION = (?:_\(u|u|)((?:\"\"\"|\'\'\'|\"|\'))(.*?)\1', re.DOTALL)
 
 
-def get_data(filePath):
+def get_data(filepath):
     """
     Extract usable information from plugin files.
     """
     data = {}
-    descLines = []
-    descFlag = False
+    desc_lines = []
+    desc_flag = False
 
-    with open(filePath) as f:
+    with open(filepath) as f:
         for line in f:
             if 'name' not in data:
-                name = re.match(reName, line)
+                name = re.match(re_name, line)
                 if name:
                     data['name'] = name.group(2)
 
             if 'author' not in data:
-                author = re.match(reAuthor, line)
+                author = re.match(re_author, line)
                 if author:
                     data['author'] = author.group(2)
 
             if 'description' not in data:
-                if re.match(reDescStart, line):
-                    descFlag = True
-                elif re.match(reDescEnd, line):
-                    descFlag = False
-                    desc = re.match(reDesc, re.sub(r'[\\\n]', '', "".join(descLines)))
+                if re.match(re_desc_start, line):
+                    desc_flag = True
+                elif re.match(re_desc_end, line):
+                    desc_flag = False
+                    desc = re.match(re_desc, re.sub(r'[\\\n]', '', "".join(desc_lines)))
                     if desc:
                         data['description'] = desc.group(2)
 
-                if descFlag:
-                    descLines.append(line)
+                if desc_flag:
+                    desc_lines.append(line)
 
             if 'version' not in data:
-                ver = re.match(reVer, line)
+                ver = re.match(re_ver, line)
                 if ver:
                     data['version'] = ver.group(2)
 
             if 'api_version' not in data:
-                apiver = re.match(reAPI, line)
+                apiver = re.match(re_api, line)
                 if apiver:
                     data['api_version'] = apiver.group(2)
 
@@ -73,46 +73,46 @@ def build_json():
     """
 
     # Read the existing data
-    if os.path.isfile(plugFile):
-        plugins = json.load(open(plugFile, "r"))["plugins"]
+    if os.path.isfile(plugin_file):
+        plugins = json.load(open(plugin_file, "r"))["plugins"]
     else:
         plugins = {}
 
-    # All top level directories in plugDir are plugins
-    for dirName in os.walk(plugDir).next()[1]:
+    # All top level directories in plugin_dir are plugins
+    for dirname in os.walk(plugin_dir).next()[1]:
 
         files = {}
         data = {}
 
-        if dirName in [".git"]:
+        if dirname in [".git"]:
             continue
 
-        dirPath = os.path.join(plugDir, dirName)
-        for root, dirs, fileNames in os.walk(dirPath):
-            for fileName in fileNames:
-                ext = os.path.splitext(fileName)[1]
+        dirpath = os.path.join(plugin_dir, dirname)
+        for root, dirs, filenames in os.walk(dirpath):
+            for filename in filenames:
+                ext = os.path.splitext(filename)[1]
 
                 if ext not in [".pyc"]:
-                    filePath = os.path.join(root, fileName)
-                    md5Hash = md5(open(filePath, "rb").read()).hexdigest()
-                    files[filePath.split(os.path.join(dirPath, ''))[1]] = md5Hash
+                    file_path = os.path.join(root, filename)
+                    md5Hash = md5(open(file_path, "rb").read()).hexdigest()
+                    files[file_path.split(os.path.join(dirpath, ''))[1]] = md5Hash
 
                 if not data:
-                    data = get_data(os.path.join(plugDir, dirName, fileName))
+                    data = get_data(os.path.join(plugin_dir, dirname, filename))
 
-        if dirName in plugins:
-            print("Updating " + dirName)
+        if dirname in plugins:
+            print("Updated: " + dirname)
             if data:
                 for key, value in data.items():
-                    plugins[dirName][key] = value
-            plugins[dirName]["files"] = files
+                    plugins[dirname][key] = value
+            plugins[dirname]["files"] = files
         else:
-            print("Adding " + dirName)
+            print("Added: " + dirname)
             data['files'] = files
             data['downloads'] = 0
-            plugins[dirName] = data
+            plugins[dirname] = data
 
-    json.dump({"plugins": plugins}, open(plugFile, "w"),
+    json.dump({"plugins": plugins}, open(plugin_file, "w"),
               sort_keys=True, indent=2)
 
 
@@ -121,26 +121,26 @@ def zip_files():
     Zip up plugin folders
     """
 
-    for dirName in os.walk(plugDir).next()[1]:
-        archivePath = os.path.join(plugDir, dirName)
-        archive = zipfile.ZipFile(archivePath + ".zip", "w")
+    for dirname in os.walk(plugin_dir).next()[1]:
+        archive_path = os.path.join(plugin_dir, dirname)
+        archive = zipfile.ZipFile(archive_path + ".zip", "w")
 
-        dirPath = os.path.join(plugDir, dirName)
-        for root, dirs, fileNames in os.walk(dirPath):
-            for fileName in fileNames:
-                filePath = os.path.join(root, fileName)
-                archive.write(filePath,
-                              filePath.split(os.path.join(dirPath, ''))[1],
+        dirpath = os.path.join(plugin_dir, dirname)
+        for root, dirs, filenames in os.walk(dirpath):
+            for filename in filenames:
+                file_path = os.path.join(root, filename)
+                archive.write(file_path,
+                              file_path.split(os.path.join(dirpath, ''))[1],
                               compress_type=zipfile.ZIP_DEFLATED)
 
-        print("Created archive: " + dirName)
+        print("Created: " + dirname + ".zip")
 
 
 # The file that contains json data
-plugFile = "../plugins.json"
+plugin_file = "plugins.json"
 
 # The directory which contains plugin files
-plugDir = "../picard-plugins/plugins"
+plugin_dir = "plugins"
 
 if __name__ == '__main__':
     if 1 in sys.argv:
@@ -151,6 +151,6 @@ if __name__ == '__main__':
         elif sys.argv[1] == "zip":
             zip_files()
     else:
-            # call(["git", "pull", "-q"])
-            build_json()
-            zip_files()
+        # call(["git", "pull", "-q"])
+        build_json()
+        zip_files()
